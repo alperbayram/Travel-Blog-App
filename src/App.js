fe0212e3e59bfb5e20ceba17932e98fa056a756d
alperbayram <alperbayrm@hotmail.com>
2022-05-24 21:19:19 +0300

    :poop:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -77,6 +77,39 @@ const blogs = [
   },
 ];
 
+const blogmetro = [
+  {
+    id: 1,
+    name: "Adventure",
+    href: "#",
+    time: "08.08.2021",
+    imageSrc: "./img/metro/img.png",
+    header: "Richird Norton photorealistic rendering as real photos",
+    title:
+      "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas.",
+  },
+  {
+    id: 2,
+    name: "Adventure",
+    href: "#",
+    time: "08.08.2021",
+    imageSrc: "./img/metro/img2.png",
+    header: "Richird Norton photorealistic rendering as real photos",
+    title:
+      "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas.",
+  },
+  {
+    id: 3,
+    name: "Adventure",
+    href: "#",
+    time: "08.08.2021",
+    imageSrc: "./img/metro/img3.png",
+    header: "Richird Norton photorealistic rendering as real photos",
+    title:
+      "Progressively incentivize cooperative systems through technically sound functionalities. Credibly productivate seamless data with flexible schemas.",
+  },
+];
+
 const bigpost = "./img/bigpost.png";
 
 function Home() {
@@ -87,19 +120,18 @@ function Home() {
       </div>
       <div className="bg-white">
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-4xl font-extrabold tracking-tight text-gray-900">
+          <h2 className="text-4xl font-extrabold tracking-tight text-gray-500">
             Popular topics
           </h2>
           <div>
-            <h2 className="text-4xl font-extrabold tracking-tight text-gray-900">
+            <h2 className="text-4xl font-extrabold tracking-tight text-gray-500">
               Popular topics
             </h2>
           </div>
-
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {blogs.map((blog) => (
               <div key={blog.id} className="group relative">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-64 lg:aspect-none">
                   <img
                     src={blog.imageSrc}
                     alt={blog.name}
@@ -143,6 +175,39 @@ function Home() {
               Progressively incentivize cooperative systems through technically
               sound functionalities. The credibly productivate seamless data.
             </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-4xl font-extrabold tracking-tight text-gray-500">
+            Editor’s Pick
+          </h2>
+
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-3 xl:gap-x-8">
+            {blogmetro.map((blog) => (
+              <div key={blog.id} className="group relative " id="blogmetro">
+                <div className="w-full min-h-8 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                  <img
+                    src={blog.imageSrc}
+                    alt={blog.name}
+                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div className="blogmetrotext">
+                    <span>{blog.time}</span>
+                    <h2 className="text-lg font-semibold ">
+                      <a href={blog.href}>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {blog.header}
+                      </a>
+                    </h2>
+                    <p className="mt-1 text-sm">{blog.title}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
